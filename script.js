159a434398c404ea34877a7ad086ce079f914418
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-06-20 20:57:20 -0700

    Update CSS file

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 var userEmail = "gavrawavanniarachchi@gmail.com"
 
 const userName = "Gavrawa Thilakshana"
+const isAthunticated = false
 let userAge = 24
+const lastName = null
+const firstName = undefined
+const userData = {
+    firstName :"Gavrawa",
+    lastName : "Thilakshana",
+    age : 24
+} 
+const fruits = ["apple", "banana", "orange"]  //Array types
+const numbers = [1,2,3,4,5,6,7,8,9,10] //Array types
+const users = [
+     {id: 1, name: "Gavrawa"},
+     {id: 2, name: "Thilakshana"}, 
+     {id: 3, name: "Gavrawa Thilakshana"} //Object Array types
+    
+    ];
 
 userAge = 25
 //This is a Single line 
@@ -10,4 +26,14 @@ is a
 multi 
 line */
 
-console.log(userEmail, userName , userAge);
+
+console.log(userEmail, typeof userEmail);
+console.log(userName, typeof userName);
+console.log(userAge, typeof userAge);
+console.log(isAthunticated, typeof isAthunticated);
+console.log(lastName, typeof lastName);
+console.log(firstName, typeof firstName);
+console.log(userData, typeof userData);
+console.log(fruits, typeof fruits);
+console.log(numbers, typeof numbers);
+console.log(users, typeof users);
